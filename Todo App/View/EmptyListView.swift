aa23fb75db5a1af2abcaa425b6d2f3d90d6eb0f4
Shakti Prasad Nayak <shakti.nayak@yusata.com>
2024-10-29 16:33:32 +0530

    Commit for change the UI/UX of todo app

---
+++ Todo App/View/EmptyListView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyListView.swift
+//  Todo App
+//
+//  Created by Shakti on 28/10/24.
+//
+
+import SwiftUI
+
+struct EmptyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyListView()
+}
